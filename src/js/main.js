8d0feb8f0eb5dea0f2d779eedb786d0fb3b53ad7
Alexey <alexeyriabinin@gmail.com>
2026-02-20 10:56:51 +0200

    зроблено desktop версію блоку hero,зроблено свайпер та анімацію на зображення при наведенні

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,24 @@
 import Swiper from 'swiper'; 
 import { Navigation, Pagination } from 'swiper/modules'; 
 import 'swiper/css'; 
-
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import "/src/sass/ui/style.scss"; 
+
+
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  slidesPerView: 1, // СУВОРО ОДНА картинка на екрані!
+     // Наступні картинки будуть заховані (прозорі)
+
+  
+  loop: true,
+  speed: 600,
+  grabCursor: true,
+  
+  pagination: {
+    el: '.hero__dots',
+    clickable: true,
+  },
+});
