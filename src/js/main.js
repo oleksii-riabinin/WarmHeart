d43d50c267d6424a3030d5f65b54aff08783c512
Alexey <alexeyriabinin@gmail.com>
2026-03-02 17:10:42 +0200

    адаптував блок products на dekstope та на ширині екрану 1500px

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ import 'swiper/css/navigation';
 import "/src/sass/ui/style.scss"; 
 
 
-const swiper = new Swiper('.swiper', {
+const swiperHero = new Swiper('.hero__slider', {
   // configure Swiper to use modules
   modules: [Navigation, Pagination],
   slidesPerView: 1, // СУВОРО ОДНА картинка на екрані!
@@ -22,5 +22,26 @@ const swiper = new Swiper('.swiper', {
   pagination: {
     el: '.hero__dots',
     clickable: true,
+  },
+});
+
+const swiperProducts = new Swiper('.products__swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  slidesPerView: 3, // СУВОРО ОДНА картинка на екрані!
+     // Наступні картинки будуть заховані (прозорі)
+
+  spaceBetween: 30,
+  loop: true,
+  speed: 600,
+  grabCursor: true,
+  
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.arrow-right',
+    prevEl: '.arrow-left',
   },
 });
